204faf0082ca143d80f5d1c4cac2b67f1989eabc
Roberto Giordano Estrada Leyva <leyva.r@samsung.com>
2020-05-09 12:27:36 -0400

    Begin Dots voxels instantiate implementation

---
+++ Assets/MyAssets/Scripts/DOTS/Components/VoxelsInitializer.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public class VoxelsInitializerComponent : IComponentData
+{
+    public Entity VoxelsEntity;
+    public float3 VolumeBoundingBoxSize;
+}
---
+++ Assets/MyAssets/Scripts/DOTS/Systems/VoxelSpawnSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class VoxelSpawnSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity e, in VoxelsInitializerComponent vi) => { });
+        /*
+        Entities
+            .ForEach((in VoxelsInitializerComponent voxelsInitializer) =>
+            {
+                var bounds = voxelsInitializer.VolumeBoundingBoxSize;
+                for(var z = 0; z < bounds.z; z++)
+                {
+                    for(var y = 0; y < bounds.y; y++)
+                    {
+                        for(var x = 0; x < bounds.x; x++)
+                        {
+                            var voxel = EntityManager.Instantiate(voxelsInitializer.VoxelsEntity);
+                            EntityManager.SetComponentData<Translation>(voxel, new Translation
+                            {
+                                Value = new float3(x, y, z)
+                            });
+                        }
+                    }
+                }
+            });/*
+    }
+    
+}
